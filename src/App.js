6437fb77b449b81fc80474bddf81e2b2958a218c
sagi barshai <sagi@sgys-MacBook-Air.local>
2024-03-08 15:34:01 +0200

    1.0.0

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        some first change!
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-         ! Learn React
+         some first change!
         </a>
       </header>
     </div>
