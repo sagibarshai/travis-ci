9ae8925fcad9904337d71e28289c0537780cc867
sagi barshai <sagi@sgys-MacBook-Air.local>
2024-03-08 19:51:41 +0200

    1.0.0

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         some first change! @
+         some first change! 
         </a>
       </header>
     </div>
